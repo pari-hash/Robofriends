174eb9c22ceae5e23b050b9950eda3577f54dafe
Paridhi <singhalparidhi9@gmail.com>
2022-02-06 11:33:27 +0530

    Updates

---
+++ precache-manifest.2e122efb6870899e960034956ac96206.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52dc10bcd6b83c93d633871482376a63",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "440b502531a35920aa02",
+    "url": "/Robofriends/static/css/2.3b2cfc2e.chunk.css"
+  },
+  {
+    "revision": "d7835df04aaf19abab7f",
+    "url": "/Robofriends/static/css/main.51231662.chunk.css"
+  },
+  {
+    "revision": "440b502531a35920aa02",
+    "url": "/Robofriends/static/js/2.0bed7501.chunk.js"
+  },
+  {
+    "revision": "d7835df04aaf19abab7f",
+    "url": "/Robofriends/static/js/main.737ec4a3.chunk.js"
+  },
+  {
+    "revision": "47afcf7c1fdce8fc928a",
+    "url": "/Robofriends/static/js/runtime~main.4fea8ee0.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
